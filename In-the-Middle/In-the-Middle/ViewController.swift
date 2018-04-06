0359bc8451cfe5a4792910a9444d72c43f12f7d1
Justin Abarquez <justincabarquez@gmail.com>
2018-04-05 18:58:30 -0700

    Displaying Map View with stylized GoogleMap and Bar. Displaying coordinates of Bay Area, CA.

--- In-the-Middle/In-the-Middle/ViewController.swift
+++ In-the-Middle/In-the-Middle/ViewController.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import CoreLocation
+import GoogleMaps
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    var minimalRetroStyle: GMSMapStyle!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //setting ViewController as a delegate of locationManager
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        
+        //Google Maps API
+        GMSServices.provideAPIKey("AIzaSyBJmHjPpr2ijDFSQjG-AUXC3r9Re4K2uGA")
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 37.7749, longitude: -122.478, zoom: 10.25)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        minimalRetroStyle = try! GMSMapStyle.init(contentsOfFileURL: Bundle.main.url(forResource: "minimalRetro", withExtension: "json")!)
+        mapView.mapStyle = minimalRetroStyle
+        view = mapView
+        
+        //        let locationA = CLLocationCoordinate2DMake(37.723324, -122.478082)
+        //        let markerA = GMSMarker(position: locationA)
+        //        markerA.title = "San Francisco State University"
+        //        markerA.map = mapView
+        //
+        //        let locationB = CLLocationCoordinate2DMake(37.6879, -122.4702)
+        //        let markerB = GMSMarker(position: locationB)
+        //        markerB.title = "Berkeley"
+        //        markerB.map = mapView
+        
+        //bar properties
+        navigationItem.title = "DINE in the MIDDLE"
+        self.navigationController?.navigationBar.barStyle  = UIBarStyle.black
+        self.navigationController?.navigationBar.barTintColor   = UIColor(red: 153/255, green: 204/255, blue: 0/255, alpha: 1.0)
+        self.navigationController?.view.backgroundColor = .clear
+        self.navigationController?.navigationBar.isTranslucent = true
+        let navigationTitleFont = UIFont(name: "Avenir", size: 22)!
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.font: navigationTitleFont]
+        
+        
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
 
