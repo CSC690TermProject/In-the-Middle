5155118b0194ba931d70cb6f7c173b43c9e14e24
Justin Abarquez <justinchristopher@Justins-MacBook-Air.local>
2018-04-05 18:30:27 -0700

    Installed Cocoapods w/ Podfile

---
+++ In-the-Middle/In-the-Middle/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  In-the-Middle
+//
+//  Created by Justin Abarquez on 4/5/18.
+//  Copyright © 2018 Justin Abarquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
